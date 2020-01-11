168e5312c78c8e455d4def8d91e25852f35da5e0
Yoshi Tashiro <tashiro@quartile.co>
2020-01-11 22:31:33 +0800

    fix error on creating employee, costmetics

--- l10n_jp_hr_employee/models/hr_employee.py
+++ l10n_jp_hr_employee/models/hr_employee.py
@@ -786,7 +786,7 @@ class HrEmployee(models.Model):
                     # ref: https://www.w3.org/TR/html5/forms.html#valid-e-mail-address
                     r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
                     rec.private_email):
-                raise ValidationError(msg % ("Private Email"))
+                raise ValidationError(msg % (_("Private Email")))
 
     def action_draft(self):
         return self.write({'state': 'draft'})
@@ -797,13 +797,15 @@ class HrEmployee(models.Model):
     def action_confirm(self):
         return self.write({'state': 'confirm'})
 
-    @api.model
-    def create(self, vals):
-        res = super(HrEmployee, self).create(vals)
-        users = self.env.ref(
-            'l10n_jp_hr_employee.group_employee_private_info').users
-        if users:
-            res.message_subscribe_users(user_ids=users.ids)
+    def _message_auto_subscribe_followers(self, updated_values, subtype_ids):
+        res = super(HrEmployee, self)._message_auto_subscribe_followers(
+            updated_values, subtype_ids
+        )
+        if updated_values.get("name") or updated_values.get("state"):
+            users = self.env.ref(
+                'l10n_jp_hr_employee.group_employee_private_info').users
+            for user in users:
+                res.append((user.partner_id.id, subtype_ids, False))
         return res
 
     def write(self, vals):
