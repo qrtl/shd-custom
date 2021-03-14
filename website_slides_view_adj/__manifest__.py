ec24545769bd91d4194a72803579d80cc587063e
Yoshi Tashiro <tashiro@quartile.co>
2021-03-14 14:38:48 +0800

    [ADD] website_slides_view_adj

---
+++ website_slides_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Slides View Adjustments",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["website_slides"],
+    "data": ["views/slide_question_views.xml"],
+}
