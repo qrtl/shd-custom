237a92f5e97222375340bfb462a99a96180a876b
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-03-15 15:28:45 +0900

    st1 [1668][ADD] http_routing_view_adj (#9)

---
+++ http_routing_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "HTTP Routing View Adjustments",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["http_routing"],
+    "data": ["views/http_rounting_template.xml"],
+}
---
+++ http_routing_view_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Adjusts 403 message.
