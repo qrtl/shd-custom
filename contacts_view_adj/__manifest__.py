ee1c8fc0bcc94dc70e8622814ba5e5f6abaf8b92
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-11-25 15:22:54 +0800

    Generate readme

---
+++ contacts_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Adjustments on Contact Views",
+    "version": "14.0.1.0.0",
+    "category": "HR",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["contacts"],
+    "data": ["views/contact_views.xml"],
+}
---
+++ contacts_view_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adjusts Contacts views.
