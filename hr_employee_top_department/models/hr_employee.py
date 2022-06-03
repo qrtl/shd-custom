4d4e84b741a9b5df20763514c6cbd81e9c6af9a2
kakurai8 <80655511+kakurai8@users.noreply.github.com>
2022-06-03 13:22:52 +0900

    st1 [2129][ADD] hr_employee_top_department (#17)
    [2129](https://www.quartile.co/web?#id=2129&action=771&model=project.task&view_type=form&menu_id=505)
    
    Target:
    - Add the name of the person's company (the highest division traced to the department) as an entry.
    
    Purpose:
    - for sending Contract Renewal Mail
    - The acquisition of the president of the person's company (the manager of the top division) is obtained through a mail template function.

---
+++ hr_employee_top_department/models/hr_employee.py
@@ -0,0 +1,28 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, fields, models
+
+
+class HrEmployee(models.Model):
+    _inherit = ["hr.employee"]
+
+    top_dept_id = fields.Many2one(
+        "hr.department",
+        string="Top Department",
+        compute="_compute_top_department",
+        help="Company",
+    )
+
+    @api.depends("department_id")
+    def _compute_top_department(self):
+        for employee in self:
+            employee.top_dept_id = False  # たしか14.0だとFalseの場合でも明示的に指定が必要なのではじめにこの行を設けておく
+            top_dept = employee.department_id
+            if not top_dept:
+                continue
+            parent = top_dept.parent_id
+            while parent:
+                top_dept = parent
+                parent = top_dept.parent_id
+            employee.top_dept_id = top_dept
---
+++ hr_employee_top_department/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module does the following:
+
+- Add Top Department to HR Employee.
+- The purpose is to obtain the name of the company and its representatives in order to contact them for contract renewal.
